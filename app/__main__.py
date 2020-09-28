b4a6fb8460043658afb8c4075ec948107fef747a
Vadim Korovin <vmkorovin7@gmail.com>
2020-09-28 14:57:35 +0300

    update_refresh

--- app/__main__.py
+++ app/__main__.py
@@ -1,20 +1,37 @@
-import random
+
 import time
+import requests
+import os
+import dotenv
 
 from appscript import app, mactypes
+dotenv.load_dotenv()
 
-from .common import IMAGES_PATH
 
 
 def choose_background() -> str:
     """Randomly choose a new image for the background."""
-    paths = list(IMAGES_PATH.iterdir())
-    path = random.choice(paths)
+    # TODO:
+    UNSPLASH_ACCESS_KEY = os.environ['UNSPLASH_ACCESS_KEY']
+    host = 'https://api.unsplash.com'
+    endpoint = '/photos/random'
+    url = f'{host}{endpoint}' # use Unsplash API to download random image
+    headers = {'Authorization': 'Client-ID {}'.format(UNSPLASH_ACCESS_KEY)
+}
+    response = requests.get(url, headers=headers)
+    json_response = response.json()
+    link = json_response['urls']['full']
+    respond = requests.get(link)
+    with open(r'/Users/vadimkorovin/projects/refresh-background/images/Whatever_image.jpg', 'wb') as f:
+        f.write(respond.content) # download image in to images dir
+    path = '/Users/vadimkorovin/projects/refresh-background/images/Whatever_image.jpg' # download image in to images dir
+
     return str(path)
 
 
 def set_background(path: str) -> None:
-    # TODO: check
+    # TODO: check v
+
     app("Finder").desktop_picture.set(mactypes.File(path))
 
 
